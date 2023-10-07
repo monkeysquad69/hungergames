c2ad24311113311d91a5cf44a1f0d1c9a224c573
iceandollie <icefighter86@gmail.com>
2023-10-07 21:12:00 +0200

    swords

--- src/main/java/com/monkeysquad/hungergames/HungerGamesDataGenerator.java
+++ src/main/java/com/monkeysquad/hungergames/HungerGamesDataGenerator.java
@@ -1,11 +1,17 @@
 package com.monkeysquad.hungergames;
 
+import com.monkeysquad.hungergames.datagen.*;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class HungerGamesDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
